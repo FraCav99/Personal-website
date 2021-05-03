7c9b6565f62d6fe1e9e03c2f2e6711a88f1f82af
FraCav99 <MasterCaster36@outlook.it>
2021-05-03 16:56:09 +0200

    Adjusted navbar and add Hero section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import '../styles/index.scss';
 import Navbar from './Navbar';
+import Hero from './Hero';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 };
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1>
+        Hello, I'm <span>Francesco</span>!
+      </h1>
+      <p>
+        A junior web developer, based in Italy. I like to reflect my ideas on
+        the web!
+      </p>
+      <button>Get in touch!</button>
+    </div>
+  );
+};
+
+export default Hero;
